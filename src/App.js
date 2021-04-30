8d9e74a508aeec21483ed6c42d63a6c7afd6023e
WebDevJ <WebDevJ@users.noreply.github.com>
2021-04-30 12:09:42 -0400

    search component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search. . ."
+          />
+        </div>
+      </main>
     </div>
   );
 }
